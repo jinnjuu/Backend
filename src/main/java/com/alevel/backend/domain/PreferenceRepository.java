eff9e9db2f4a398b966e710d83510038dd93eadc
jinnjuu <kjinnjuu@gmail.com>
2022-07-19 00:55:03 +0900

    Revert "feat:> 술 취향 등록 #5"
    This reverts commit 6ce273a51be5b6eaa10b45601ea1e267d27b4b90.

--- src/main/java/com/alevel/backend/domain/PreferenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alevel.backend.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PreferenceRepository extends JpaRepository<Preference, Long> {
-    Preference save(Preference preference);
-}
