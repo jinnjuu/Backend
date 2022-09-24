a3849d3ce4d1a511db76b20949020eda7679a5b7
Jinju Kim <44694206+jinnjuu@users.noreply.github.com>
2022-09-24 22:45:57 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURRENT_PID=$(pgrep -f java) 
+echo "CURRENT_PID: $CURRENT_PID"
+
+if [ $CURRENT_PID ]; then
+        echo "kill -9 $CURRENT_PID"
+        kill -9 $CURRENT_PID
+else
+        echo "실행중인 프로세스가 없습니다."
+fi
+
+sleep 5
+
+cd ~/test
+JAR_NAME=$(ls | grep backend)
+echo "JAR_NAME: $JAR_NAME"
+
+nohup java -jar $JAR_NAME > /dev/null 2>&1 &
