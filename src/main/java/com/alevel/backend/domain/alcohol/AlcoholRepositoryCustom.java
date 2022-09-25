dbb9e37b989851e18c45340bb84a2e1033df7e0e
jinnjuu <kjinnjuu@gmail.com>
2022-09-25 15:43:05 +0900

    refactor:> 술 전체 조회 시 술 데이터만 출력 #42

--- src/main/java/com/alevel/backend/domain/alcohol/AlcoholRepositoryCustom.java
+++ src/main/java/com/alevel/backend/domain/alcohol/AlcoholRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.alevel.backend.domain.alcohol;
 
+import com.alevel.backend.controller.dto.AlcoholResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AlcoholRepositoryCustom {
 
-    Page<Alcohol> findAllAlcohol(String type, String category, Pageable pageable);
+    Page<AlcoholResponseDto> findAllAlcohol(String type, String category, Pageable pageable);
 
 }
